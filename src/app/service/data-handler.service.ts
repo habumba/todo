18083bd3ef95f88c789a279c79d55f07cd029339
Habumba <geosoft@inbox.ru>
2020-05-19 20:12:44 +0300

    BehaviorSubject (все задачи при открытии формы)

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -2,14 +2,15 @@ import { Injectable } from '@angular/core';
 import {Category} from "../model/Category";
 import {TestData} from "../data/TestData";
 import {Task} from "../model/Task";
-import {Subject} from "rxjs";
+import {BehaviorSubject, Subject} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataHandlerService {
 
-  tasksSubject = new Subject<Task[]>();
+  tasksSubject = new BehaviorSubject<Task[]>(TestData.tasks);
+  categoriesSubject = new BehaviorSubject<Category[]>(TestData.categories);
 
   constructor() { }
 

33b45bc58183d7f36c236c2d3a417b370fae6b87
Habumba <geosoft@inbox.ru>
2020-05-19 21:31:45 +0300

    подсвечивается только выбранная категория

--- src/app/views/categories/categories.component.ts
+++ src/app/views/categories/categories.component.ts
@@ -11,6 +11,8 @@ export class CategoriesComponent implements OnInit {
 
     categories: Category[];
 
+    selectedCategory: Category;
+
     constructor(private dataHandler: DataHandlerService) {
     }
 
@@ -21,6 +23,7 @@ export class CategoriesComponent implements OnInit {
     }
 
     showTasksByCategory(category: Category) {
+        this.selectedCategory = category;
         this.dataHandler.fillTasksByCategory(category);
     }
 }

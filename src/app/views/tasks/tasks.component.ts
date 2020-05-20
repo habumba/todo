c4952b9230ba62ad357a45a1806df91dfd308d0b
Habumba <geosoft@inbox.ru>
2020-05-20 20:58:31 +0300

    сортировка и постраничность

--- src/app/views/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks.component.ts
@@ -1,18 +1,24 @@
-import {Component, OnInit} from '@angular/core';
+import {AfterViewInit, Component, OnInit, ViewChild} from '@angular/core';
 import {DataHandlerService} from "../../service/data-handler.service";
 import {Task} from 'src/app/model/Task';
 import {MatTableDataSource} from "@angular/material";
+import {MatPaginator} from "@angular/material/paginator";
+import {MatSort} from "@angular/material/sort";
 
 @Component({
     selector: 'app-tasks',
     templateUrl: './tasks.component.html',
     styleUrls: ['./tasks.component.css']
 })
-export class TasksComponent implements OnInit {
+export class TasksComponent implements OnInit, AfterViewInit {
 
     // поля для таблицы (те, что отображают данные из задачи - должны совпадать с названиями переменных класса)
     private displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
     private dataSource: MatTableDataSource<Task>; // контейнер - источник данных для таблицы
+
+    // ссылки на компоненты таблицы
+    @ViewChild(MatPaginator, {static: false}) private paginator: MatPaginator;
+    @ViewChild(MatSort, {static: false}) private sort: MatSort;
 
 
     tasks: Task[];
@@ -27,6 +33,13 @@ export class TasksComponent implements OnInit {
         this.dataSource = new MatTableDataSource();
 
         this.refreshTable();
+    }
+
+    // в этом методе уже все проинциализировано, поэтому можно присваивать объекты (иначе может быть ошибка undefined)
+    ngAfterViewInit(): void {
+
+        this.addTableObjects();
+
     }
 
 
@@ -55,6 +68,36 @@ export class TasksComponent implements OnInit {
 
         this.dataSource.data = this.tasks; // обновить источник данных (т.к. данные массива tasks обновились)
 
+        this.addTableObjects();
 
+
+        // когда получаем новые данные..
+        // чтобы можно было сортировать по столбцам "категория" и "приоритет", т.к. там не примитивные типы, а объекты
+        // @ts-ignore - показывает ошибку для типа даты, но так работает, т.к. можно возвращать любой тип
+        this.dataSource.sortingDataAccessor = (task, colName) => {
+
+            // по каким полям выполнять сортировку для каждого столбца
+            switch (colName) {
+                case 'priority': {
+                    return task.priority ? task.priority.id : null;
+                }
+                case 'category': {
+                    return task.category ? task.category.title : null;
+                }
+                case 'date': {
+                    return task.date ? task.date : null;
+                }
+
+                case 'title': {
+                    return task.title;
+                }
+            }
+        };
+
+    }
+
+    private addTableObjects() {
+        this.dataSource.sort = this.sort; // компонент для сортировки данных (если необходимо)
+        this.dataSource.paginator = this.paginator; // обновить компонент постраничности (кол-во записей, страниц)
     }
 }
